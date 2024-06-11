16c633f377f4373e3cfee1a6c4a9612e6bcd2566
Paa Kwesi Ephraim <peekaylink@gmail.com>
2024-06-11 16:21:02 +0000

    updated

--- lab1.js
+++ lab1.js
@@ -73,7 +73,7 @@ const sum = (arr) => {
   if (!arr) {
     return "invalid input"
   }
-  arr.reduce((acc, num) => acc + num, 0)
+  return arr.reduce((acc, num) => acc + num, 0)
 }
 console.log(sum([1, 2, 3, 4, 5, 6, 7, 8]))
 
